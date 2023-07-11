1364045b4f253bd13da37d2c0283e73effc098be
manikanta9578 <manikanta9578@gmail.com>
2023-07-11 15:27:09 +0530

    added middleware

--- server/server.js
+++ server/server.js
@@ -2,28 +2,29 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const { connectDB } = require("./config/db");
 const busRoute = require("./routes/bus");
 const stripeRoute = require("./routes/stripe");
 const receiptRoute = require("./routes/receipt");
+const userRoute = require('./routes/userRoutes');
+
+connectDB();
 
 var cors = require('cors');
 app.use(cors())
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then(() => console.log("DB connection successfull!"))
-  .catch((error) => {
-    console.log(error);
-  });
-
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use("/api/buses", busRoute);
 app.use("/api/stripe", stripeRoute);
 app.use("/api/receipt", receiptRoute);
+
+app.use('/api/users', userRoute);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(process.env.PORT, (err) => {
   if (err) console.log(err);
