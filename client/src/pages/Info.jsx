42df7965b81257dccb0d2e8045ec40074f837d8e
manikanta9578 <manikanta9578@gmail.com>
2023-07-11 22:56:14 +0530

    navigate added

--- client/src/pages/Info.jsx
+++ client/src/pages/Info.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 // import PassengerForm from "../components/Info/PassengerForm";
 // import ContactForm from "../components/Info/ContactForm";
 import FareDetails from "../components/Info/FareDetails";
@@ -47,7 +47,7 @@ const Info = () => {
       })
       .catch((err) => console.log(err.message));
 
-    // navigate("/receipt");
+    Navigate("/receipt");
 
     //Save receipt data to server
     const receiptData = {
